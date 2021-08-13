6556c670ca69fdbd22f4bb5404008038a4a4c1e6
David Adams <fso308@utsa.edu>
2021-08-13 17:33:36 -0500

    added some functions and for loops

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let lower = [];
+var generate = document.getElementById("generate");
 let upper = [];
-let nums = [];
+let lower = [];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 // Array to hold passwrod options
 let options = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k"
 ,"L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X",
 "x","Y","y","Z","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")"]; 
-// Creating Upper and Lower Case Arrays
-function aC(x, name)  {
-  for (var i = x; i < options.length - 20; i = i + 2)
-  name.push(options[i]);
-  return name;
+
+// Prompt for password criteria
+
+function greet() {
+  prompt("What would you like your password to include?");
 }
 
-console.log(aC(0, upper));
-console.log(aC(1, lower));
+function arrayCreator(x, name) {
+  for (x = x; x < options.length; x=x+2) {
+    
+  }  
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", greet);
