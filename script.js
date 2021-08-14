bbc05d60b91ffbbed928b32ae1d0d3061f76c4e2
DNA555 <dnadamspne@gmail.com>
2021-08-14 13:07:28 -0500

    Got the length of the password to show

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var generate = document.getElementById("generate");
+var textbox = document.getElementById("password");
 let upper = [];
 let lower = [];
 // Write password to the #password input
@@ -12,21 +13,45 @@ function writePassword() {
 }
 // Array to hold passwrod options
 let options = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k"
-,"L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X",
-"x","Y","y","Z","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")"]; 
+,"L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"];
 
 // Prompt for password criteria
+function arrayCreator(x, name) {
 
-function greet() {
-  prompt("What would you like your password to include?");
+  for (x = x; x < options.length; x=x+2) {
+    name.push(options[x]);
+  }
+  return name;
+}
+arrayCreator(0,upper);
+arrayCreator(1,lower);
+
+function random(arr) {
+  var x = arr[Math.floor(Math.random() * 26)];
+  return x;
 }
 
-function arrayCreator(x, name) {
-  for (x = x; x < options.length; x=x+2) {
+let func = {
+  l: random(upper),
+  u: random(lower),
+}
+// Asking for password stuff
+function meetAndGreet() {
+  length = [];
+  do  {
+    length == prompt("Password has to be between 8 and 128");
     
-  }  
+  } while (length < 8 || length > 128 || isNaN(length));
+
+  console.log(isNaN(length ))
+  /* do { 
+    upperYes = prompt("Do you want upper case letters: Y or N")
+  } while ();
+  */
+  // for (var i = 0; i < length.length; i++) {}
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", greet);
+
+generateBtn.addEventListener("click", meetAndGreet);
